bd7a173efa52b387a6b40b59fb386f08092ba508
Kristian <kthoms10@student.aau.dk>
2014-09-18 15:05:43 +0200

    .

--- WebCrawler/Indexer.py
+++ WebCrawler/Indexer.py
@@ -1,21 +1,13 @@
 from urllib import request
-import re
-
-
-
+from lib.dehtml import dehtml
 
 class Indexer():
     def parse_html(self, html):
-        return self.get_text_from_http(html)
-
-    def get_text_from_http(self, html):
-        print (html[:20])
-        re_text = re.compile("<p>(.+)</p>")
-        return re_text.findall(html)
-
+        return dehtml(html).replace('\\n', '').replace('\\t', '')
 
 if __name__ == "__main__":
     ind = Indexer()
     url = "http://en.wikipedia.org/wiki/Murder_of_Joanna_Yeates"
-    print("hej")
-    print(ind.parse_html(request.urlopen(url)))
+    print("testing...")
+    print(ind.parse_html(str(request.urlopen(url).read())))
+
